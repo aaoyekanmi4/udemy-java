cad0721f151597b9d971236e72519127a335d631
Arit Oyekan <aaoyekanmi4@gmail.com>
2023-09-19 17:00:56 -0500

    added combos remianing to master mind game

--- Udemy-Recursion-Backtracking-DP/src/MasterMind/FourNumberGame.java
+++ Udemy-Recursion-Backtracking-DP/src/MasterMind/FourNumberGame.java
@@ -2,6 +2,7 @@ package MasterMind;
 import Backtracking.Permutations;
 import Helpers.ConsoleIO;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -10,12 +11,13 @@ public class FourNumberGame {
     private List<Integer> code;
     private int tries = 5;
     private int currentTry = 1;
+    private List<List<Integer>> permutations;
     private boolean gameOver;
     private ConsoleIO io;
 
     private void init() {
         Permutations permute = new Permutations();
-        List<List<Integer>> permutations = permute.solve(new int[]{1,2,3,4});
+        this.permutations = permute.solve(new int[]{1,2,3,4});
         Random rand = new Random();
         int randomIdx = rand.nextInt(permutations.size());
         this.code = permutations.get(randomIdx);
@@ -41,8 +43,49 @@ public class FourNumberGame {
         }
         return rightlyPlaced;
     }
+
+    private void checkCombos(int numberRightlyPlaced, int[] guess) {
+        // go through permutations and eliminate combos it can't be and set that to the permutations
+        // if 0 are right, eliminate all combos where 1 in that place, 2 in that place, 3  in that place,
+        // or 4 in that place
+        if (numberRightlyPlaced == 0) {
+            for (int i = permutations.size()-1; i >= 0; i--) {
+                for (int j = 3; j >= 0; j--) {
+                    if (permutations.get(i).get(j) == guess[j]) {
+                        permutations.remove(i);
+                        break;
+                    }
+                }
+            }
+        } else if (numberRightlyPlaced == 1) {
+            for (int i = permutations.size()-1; i >= 0; i--) {
+                int numberMatching = 0;
+                for (int j = 3; j >= 0; j--) {
+                    if (permutations.get(i).get(j) == guess[j]) {
+                        numberMatching++;
+                    }
+                }
+                if (numberMatching > 1 || numberMatching == 0) {
+                    permutations.remove(i);
+                }
+            }
+        } else if (numberRightlyPlaced == 2) {
+            for (int i = permutations.size()-1; i >= 0; i--) {
+                int numberWrong = 0;
+                for (int j = 3; j >= 0; j--) {
+                    if (permutations.get(i).get(j) != guess[j]) {
+                        numberWrong++;
+                    }
+                }
+                if (numberWrong > 2 || numberWrong == 0) {
+                    permutations.remove(i);
+                }
+            }
+        }
+    }
+
     private void makeGuess () {
-        System.out.println("Attempt number " + currentTry);
+        System.out.println("Attempt number: " + currentTry);
         String guessInput = io.readRequiredString("Enter numbers 1-4 in any order separated by commas\n");
         int[] guess = convertGuessToInt(guessInput);
         int numberRightlyPlaced = checkGuess(guess);
@@ -51,6 +94,9 @@ public class FourNumberGame {
             System.out.println(Arrays.toString(code.toArray()) + " was the code!");
             gameOver = true;
         } else {
+            checkCombos(numberRightlyPlaced, guess);
+            System.out.println(permutations);
+            System.out.println("Combos Left: " + permutations.size());
             System.out.printf("%s number%s in the right place. Try again. %n", numberRightlyPlaced,
                     numberRightlyPlaced == 1 ? "" : "s");
             tries--;
